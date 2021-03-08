6c5a8ee17854d24252aa4e159bb07da965c16fc4
RakaPKS <raka@rpkschipperheijn.com>
2021-03-08 04:19:28 -0800

    added findMedianSortedArrays

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 import twoSum
 import lengthOfLongestSubstring
+import findMedianSortedArrays
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -9,6 +10,7 @@ import lengthOfLongestSubstring
 if __name__ == '__main__':
     print(twoSum.twoSum([3,2,4],6))
     print(lengthOfLongestSubstring.lengthOfLongestSubstring("abcabcbb"))
+    print(findMedianSortedArrays.findMedianSortedArrays([2,3,4,5,6],[2,3,6,7,9]))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
